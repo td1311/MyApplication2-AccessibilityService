e2b0cb74ab2be7f86d6573c73105665e0825ff97
td1311 <41880710+td1311@users.noreply.github.com>
2021-02-06 21:42:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/GlobalActionBarService.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class GlobalActionBarService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
