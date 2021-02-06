24dc476a6e029026e0173257246c31bc11b6d4b5
td1311 <41880710+td1311@users.noreply.github.com>
2021-02-06 22:50:25 +0700

    add Accessibility Service

--- app/src/main/java/com/example/myapplication/GlobalActionBarService.kt
+++ app/src/main/java/com/example/myapplication/GlobalActionBarService.kt
@@ -1,9 +1,42 @@
 package com.example.myapplication
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.accessibilityservice.GestureDescription.StrokeDescription
+import android.graphics.Path
+import android.graphics.PixelFormat
+import android.media.AudioManager
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import android.widget.FrameLayout
 
 class GlobalActionBarService : AccessibilityService() {
+    var mLayout: FrameLayout? = null
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        mLayout = FrameLayout(this)
+        val lp = WindowManager.LayoutParams()
+        lp.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY
+        lp.format = PixelFormat.TRANSLUCENT
+        lp.flags = lp.flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+        lp.gravity = Gravity.TOP
+        val inflater = LayoutInflater.from(this)
+        inflater.inflate(R.layout.action_bar, mLayout)
+        wm.addView(mLayout, lp)
+        configurePowerButton()
+        configureVolumeButton()
+        configureScrollButton()
+        configureSwipeButton()
+    }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
 
@@ -11,5 +44,56 @@ class GlobalActionBarService : AccessibilityService() {
 
     override fun onInterrupt() {
 
+    }
+
+    private fun configurePowerButton() {
+        val powerButton: Button = mLayout!!.findViewById<View>(R.id.power) as Button
+        powerButton.setOnClickListener {
+            performGlobalAction(GLOBAL_ACTION_POWER_DIALOG)
+        }
+    }
+
+    private fun configureVolumeButton() {
+        val volumeUpButton: Button = mLayout!!.findViewById<View>(R.id.volume_up) as Button
+        volumeUpButton.setOnClickListener {
+            val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI)
+        }
+    }
+
+    private fun configureSwipeButton() {
+        val swipeButton = mLayout!!.findViewById<View>(R.id.swipe) as Button
+        swipeButton.setOnClickListener {
+            val swipePath = Path()
+            swipePath.moveTo(500f, 500f)
+            swipePath.lineTo(50f, 500f)
+            val gestureBuilder = GestureDescription.Builder()
+            gestureBuilder.addStroke(StrokeDescription(swipePath, 0, 500))
+            dispatchGesture(gestureBuilder.build(), null, null)
+        }
+    }
+
+    private fun configureScrollButton() {
+        val scrollButton = mLayout!!.findViewById<View>(R.id.scroll) as Button
+        scrollButton.setOnClickListener {
+            val scrollable = findScrollableNode(rootInActiveWindow)
+            scrollable?.performAction(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD.id)
+        }
+    }
+
+    private fun findScrollableNode(root: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        val deque: ArrayDeque<AccessibilityNodeInfo> = ArrayDeque()
+        deque.add(root)
+        while (!deque.isEmpty()) {
+            val node: AccessibilityNodeInfo = deque.removeFirst()
+            if (node.actionList.contains(AccessibilityNodeInfo.AccessibilityAction.ACTION_SCROLL_FORWARD)) {
+                return node
+            }
+            for (i in 0 until node.childCount) {
+                deque.addLast(node.getChild(i))
+            }
+        }
+        return null
     }
 }
